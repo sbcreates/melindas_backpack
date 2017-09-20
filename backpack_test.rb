09c8373c2a6ebb116780402dcff7b21e2e6f3040
sbcreates <drawsarahb@gmail.com>
2017-09-20 11:45:06 -0400

    Refactored comments. Deleted unnecessary ones

--- backpack_test.rb
+++ backpack_test.rb
@@ -43,7 +43,7 @@ class BackpackTest < MiniTest::Test
   end
 
 
-  ###### TESTS ABOUT GYM SHOES... wait, where are the tests about gym shoes? #####
+  ###### TESTS ABOUT GYM SHOES #####
   def test_that_brings_gym_shoes_on_gym_day
     backpack = build(:backpack, day_of_week: 'monday')
     assert(backpack.items.include?('gym shoes'))
