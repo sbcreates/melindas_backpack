45f8654f1a6a166dce8b638d6d8885b2a0abaa2f
sbcreates <drawsarahb@gmail.com>
2017-09-20 11:46:48 -0400

    Renamed method for packing list summary so it's clear what method does

--- factories.rb
+++ factories.rb
@@ -2,7 +2,7 @@ require 'factory_girl'
 
 FactoryGirl.define do
   factory :backpack do
-    #just here so the initializer can behave like a rails model
+    # initializer can behave like a rails model
     initialize_with { new(attributes) }
   end
 end
